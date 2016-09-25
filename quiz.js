afe6d93a432b4a8716d7a37538b798a4304ecaf1
jessica kilby <jessicakilby@gmail.com>
2016-09-25 13:49:49 -0500

    behavior section complete

--- quiz.js
+++ quiz.js
@@ -1,5 +1,5 @@
 function IfXHRFails() {
-	var outputText = document.getElementById('output');
+	var outputText = document.getElementById("output");
 	outputText.innerHTML = `<div>We are having difficulties right now.</div>`;
 }
 
@@ -9,8 +9,7 @@ function WhenChunksArrive() {
 
 function AfterFileLoaded() {
 	var data = JSON.parse(this.responseText);
-	// console.log("data", data);
-	var outputText = document.getElementById('output');
+	var outputText = document.getElementById("output");
 	var carData = "";
 	var currentCar;
 
@@ -23,7 +22,32 @@ function AfterFileLoaded() {
 		carObjects += '<div class="description">Discription: '+currentCar.description+'</div>';
 		outputText.innerHTML += '<div class="carDiv col-xs-6 col-sm-4">'+carObjects+'</div>';
 	};
+	var theCarDivs = document.getElementsByClassName("carDiv");
+	var inputText = document.getElementById("input");
+	for (var i = 0; i < theCarDivs.length; i++) {
+		theCarDivs[i].addEventListener("click", function(event) {
+			descriptionDiv = this.childNodes[4];
+			descriptionDiv.parentElement.classList.toggle("select");
+			if(inputText.value === "Change car description"){
+		      inputText.value = "";
+		    } 
+		    else if (inputText.value === ""){
+		      inputText.value = "Change car description";
+		    }; 
+		    inputText.focus();
+		    inputText.addEventListener("keyup", function(event){
+		      descriptionDiv.innerHTML = inputText.value;
+		    });
+		});
+	}
+	function enterKeyPressed(keypress){
+	  if (keypress.which === 13) {
+	    inputText.value = "";
+	  }
+	}
+	document.addEventListener("keypress", enterKeyPressed);
 }
+
 
 
 var myRequest = new XMLHttpRequest();
